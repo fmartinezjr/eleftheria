c93978a9bb8f980d2bd2af5794f1920b80cb1848
martinezfran63 <martinezfran63@gmail.com>
2019-12-06 14:39:27 -0700

    install .env

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const port = process.env.PORT || 5001;
 
 
 app.use(express.static(path.join(__dirname, 'build')))
-
+app.use(express.static(path.join(__dirname,'/build')));
+app.get('/', function (req, res, next) {
+    res.sendFile(path.resolve('build/index.html'));
+});
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
