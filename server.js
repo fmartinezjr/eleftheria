65658be54d426fd066f74f5f9d294c47a3b524ab
martinezfran63 <martinezfran63@gmail.com>
2019-12-08 19:48:47 -0700

    add packages

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+var createError = require('http-errors');
 const app = express();
 const path = require('path')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.static(path.join(__dirname,'/build')));
@@ -13,5 +16,10 @@ app.get('/', function (req, res, next) {
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.get('/testing', (req, res) => {
+  res.send({ express: 'Tes' });
+});
+
 
 app.listen(8080)
