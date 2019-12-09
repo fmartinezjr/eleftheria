9ac50aceb4ac937f11ffaa297ce1656c1acd24bf
martinezfran63 <martinezfran63@gmail.com>
2019-12-08 19:59:21 -0700

    use express routes file

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 var createError = require('http-errors');
-const app = express();
 const path = require('path')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes')
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.static(path.join(__dirname,'/build')));
-app.get('/', function (req, res, next) {
-    res.sendFile(path.resolve('build/index.html'));
-});
-
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
-
-app.get('/testing', (req, res) => {
-  res.send({ express: 'Tes' });
-});
-
 
 app.listen(8080)
+
+app.use('/', indexRouter)
+
+module.exports = app;
