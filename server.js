ec5ae5d10adad7a3269c6a6914262903aea882b6
martinezfran63 <martinezfran63@gmail.com>
2019-12-10 11:56:05 -0700

    define counter metric, define histogram, define metrics endpoint

--- server.js
+++ server.js
@@ -3,6 +3,32 @@ var createError = require('http-errors');
 const path = require('path')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const client = require('prom-client');
+
+//get extra metrc from the server itself
+const collectDefaultMetrcs = client.collectDefaultMetrics;
+
+//How often it is probing
+collectDefaultMetrcs({ timeout: 5000 });
+
+//define the counter metrc (number of operations)
+const counter = new client.Counter({
+    name: 'node_request_operations_total',
+    help: 'The total number of processed requests'
+});
+
+//define the histogram (duration)
+const histogram = new client.Histogram({
+    name: 'node_request_duration_seconds',
+    help: 'Histogram for the duration in seconds.',
+    buckets: [1, 2, 5, 6, 10]
+});
+
+// Metrics endpoin
+app.get('/metrcs', (req, res) => {
+    res.set('Content-Type', client.register.contentType)
+    res.end(client.register.metrics())
+})
 
 var indexRouter = require('./routes')
 
@@ -16,8 +42,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.static(path.join(__dirname,'/build')));
 
-app.listen(5001)
+app.listen(8080)
 
 app.use('/', indexRouter)
+
+
 
 module.exports = app;
