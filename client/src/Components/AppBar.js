722472844ce43c1e4e0ed9426ee6e08d5608c95e
martinezfran63 <martinezfran63@gmail.com>
2019-12-11 12:22:54 -0700

    move tabs to nav.js

--- client/src/Components/AppBar.js
+++ client/src/Components/AppBar.js
@@ -3,11 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-
-
-
+import Nav from './Nav';
 
 
 const useStyles = makeStyles(theme => ({
@@ -25,19 +21,6 @@ const useStyles = makeStyles(theme => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
-  function LinkTab(props) {
-    return (
-      <Tab
-        component="a"
-        onClick={event => {
-          event.preventDefault();
-        }}
-        {...props}
-      />
-    );
-  }
-  
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -45,17 +28,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
            eleftheria
           </Typography>
-          <Tabs
-          variant="fullWidth"
-          aria-label="nav tabs example"
-        >
-          <LinkTab label="Home" href="/" />
-          <LinkTab label="Auto" href="/auto"  />
-          <LinkTab label="Investment" href="/investment"  />
-          <LinkTab label="Real Estate" href="/realestate"  />
-          <LinkTab label="Tax" href="/tax"  />
-          <LinkTab label="Retirement" href="/retirement"  />
-        </Tabs>
+          <Nav></Nav>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,30 +1,43 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 
 class Nav extends React.Component {
     render() {
+
+
+  function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={event => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+
+  
         return (
         <div>
-            <ul>
-                <li>
-                <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/auto">Auto</Link>
-                </li>
-                <li>
-                <Link to="/investment">Investment</Link>
-                </li>
-                <li>
-                <Link to="/realestate">Real Estate</Link>
-                </li>
-                <li>
-                <Link to="/tax">Tax</Link>
-                </li>
-                <li>
-                <Link to="/retirement">Retirement</Link>
-                </li>
-            </ul>
+        <Tabs
+          variant="fullWidth"
+          aria-label="nav tabs example"
+        >
+          <LinkTab label="Home" href="/" />
+          <LinkTab label="Auto" href="/auto"  />
+          <LinkTab label="Investment" href="/investment"  />
+          <LinkTab label="Real Estate" href="/realestate"  />
+          <LinkTab label="Tax" href="/tax"  />
+          <LinkTab label="Retirement" href="/retirement"  />
+        </Tabs>
         </div>
         )
       }
