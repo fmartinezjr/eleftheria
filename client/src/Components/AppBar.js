2674db28a459bfd25513e96afc67c7def380e857
martinezfran63 <martinezfran63@gmail.com>
2019-12-11 12:07:38 -0700

    add correct title and link to linktab

--- client/src/Components/AppBar.js
+++ client/src/Components/AppBar.js
@@ -6,6 +6,14 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Nav from './Nav';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,6 +30,19 @@ const useStyles = makeStyles(theme => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+  function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={event => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+  
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -29,7 +50,17 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
            eleftheria
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Tabs
+          variant="fullWidth"
+          aria-label="nav tabs example"
+        >
+          <LinkTab label="Home" href="/" />
+          <LinkTab label="Auto" href="/auto"  />
+          <LinkTab label="Investment" href="/investment"  />
+          <LinkTab label="Real Estate" href="/realestate"  />
+          <LinkTab label="Tax" href="/tax"  />
+          <LinkTab label="Retirement" href="/retirement"  />
+        </Tabs>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/Components/Layout.js
+++ client/src/Components/Layout.js
@@ -7,7 +7,7 @@ class Layout extends React.Component {
         return (
         <div>
             <AppBar></AppBar>
-            <Nav></Nav>
+
         </div>
         )
       }
