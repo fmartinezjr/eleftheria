4164cb8c7901cd1ed76e29882dadde1c5098f040
martinezfran63 <martinezfran63@gmail.com>
2019-12-11 12:19:38 -0700

    remove unused imports gfrom appbar.js

--- client/src/Components/AppBar.js
+++ client/src/Components/AppBar.js
@@ -3,13 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import PropTypes from 'prop-types';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Box from '@material-ui/core/Box';
+
 
 
 
