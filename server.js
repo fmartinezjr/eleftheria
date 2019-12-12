edf97480c59da4d2762120e435c1c76efae2b46c
martinezfran63 <martinezfran63@gmail.com>
2019-12-11 21:36:49 -0700

    prettier

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require('express');
-var createError = require('http-errors');
-const path = require('path')
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+var createError = require("http-errors");
+const path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes')
+var indexRouter = require("./routes");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, "/build")));
 
-app.use('/api', indexRouter)
+app.use("/api", indexRouter);
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-app.listen(8080)
+app.listen(8080);
 module.exports = app;
