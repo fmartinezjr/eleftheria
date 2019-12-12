73824948106bfe816051df9e5ad345fb805b0508
martinezfran63 <martinezfran63@gmail.com>
2019-12-12 13:12:07 -0700

    api to front end

--- client/src/Auto/Auto.js
+++ client/src/Auto/Auto.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Layout from "../Components/Layout.js";
+import axios from "axios";
 
 class Auto extends React.Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const posts = res.data;
+        this.setState({ posts });
+      })
+  }
+
+
   render() {
     return (
       <div>
         <Layout></Layout>
+        <ul>
+        { this.state.posts.map(posts => <li>{posts.name}</li>)}
+      </ul>
       </div>
     );
   }
--- routes.js
+++ routes.js
@@ -5,8 +5,12 @@ router.get("/hello", (req, res) => {
   res.json("on route 1 -> /api/hello");
 });
 
-router.get("/hello2", (req, res) => {
-  res.json("on route two");
-});
+router.get('/api/get/allposts', (req, res, next ) => {
+  pool.query(`SELECT * FROM posts 
+              ORDER BY date_created DESC`, 
+            (q_err, q_res) => {
+                  res.json(q_res.rows)
+  })
+})
 
 module.exports = router;
