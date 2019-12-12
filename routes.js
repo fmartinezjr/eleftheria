04192a1d974cd1455a16f2cefab81b609af5e4bb
martinezfran63 <martinezfran63@gmail.com>
2019-12-12 14:10:33 -0700

    import db object into routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var pool = require("./db");
 
 router.get("/hello", (req, res) => {
   res.json("on route 1 -> /api/hello");
 });
 
-router.get('/api/get/allposts', (req, res, next ) => {
-  pool.query(`SELECT * FROM posts 
-              ORDER BY date_created DESC`, 
+router.get('/get/users', (req, res, next ) => {
+  pool.query(`SELECT * FROM users`, 
             (q_err, q_res) => {
                   res.json(q_res.rows)
   })
