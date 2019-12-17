6aea8935271d6ec393da55b41bf108e671abb89d
martinezfran63 <martinezfran63@gmail.com>
2019-12-17 16:42:47 -0700

    add message for invalid routes

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 var pool = require("./db");
 
+router.get('*', (req, res) => res.status(200).send({
+  message: 'This is not a valid route',
+}));
 
 router.get('/get/transaction', (req, res, next ) => {
   pool.query(`SELECT * FROM transaction`, 
@@ -9,6 +12,17 @@ router.get('/get/transaction', (req, res, next ) => {
                   res.json(q_res.rows)
   })
 })
+
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
 
 
 module.exports = router;
