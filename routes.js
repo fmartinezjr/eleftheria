fc8b71e8b4b8e020760914696b0e83d37740f7e2
martinezfran63 <martinezfran63@gmail.com>
2019-12-17 22:59:47 -0700

    error handling

--- routes.js
+++ routes.js
@@ -2,17 +2,20 @@ var express = require("express");
 var router = express.Router();
 var pool = require("./db");
 
+router.get("/get/transaction", (req, res, next) => {
+  pool.query(`SELECT * FROM transaction`, (err, q_res) => {
+    if (err) {
+      next(err); // Pass errors to Express.
+    } else {
+      res.json(q_res.rows);
+    }
+  });
+});
 
-router.get('/get/transaction', (req, res, next ) => {
-  pool.query(`SELECT * FROM transaction`, 
-            (q_err, q_res) => {
-                  res.json(q_res.rows)
+router.get("*", (req, res) =>
+  res.status(200).send({
+    message: "This is not a valid route"
   })
-})
-
-router.get('*', (req, res) => res.status(200).send({
-  message: 'This is not a valid route',
-}));
-
+);
 
 module.exports = router;
