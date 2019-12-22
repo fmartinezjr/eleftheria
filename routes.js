c722c724dd115c00f21b0ab24dc550cf6ebe08e1
martinezfran63 <martinezfran63@gmail.com>
2019-12-21 20:59:36 -0700

    assign post parameters to req.query

--- routes.js
+++ routes.js
@@ -3,24 +3,30 @@ var router = express.Router();
 var pool = require("./db");
 
 router.get("/get/transaction", (req, res, next) => {
+  console.log(res.body);
   pool.query(`SELECT * FROM transaction`, (err, data) => {
     if (err) {
       next(err)
     } else {
       res.json(data.rows);
+      console.log(data.rows);
     }
   });
 });
 
 router.post("/post/transaction", (req, res, next) => {
 
-  const { merchant, payment_type, amount} = req.body;
+  const { merchant, payment_type, amount } = req.query;
 
   pool.query(`INSERT INTO transaction (merchant, payment_type, amount) VALUES ($1, $2, $3)`, [merchant, payment_type, amount], (err, results) => {
     if (err) {
       next(err)
     } else {
-      res.status(201).send(`Added:`);
+      res.status(201).send(`Added the following:
+      Merchant: ${req.query.merchant}
+      Payment: ${req.query.payment_type}
+      Amount: ${req.query.amount}`)
+      console.log("1 record inserted");
     }
   });
 });
