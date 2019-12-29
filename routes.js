9faebaa39af56671176dc8ea2e9eebc4650af040
martinezfran63 <martinezfran63@gmail.com>
2019-12-29 13:22:35 -0700

    delete endpoint. wip

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var pool = require("./db");
 
 router.get("/get/transaction", (req, res, next) => {
-  pool.query(`SELECT * FROM transaction`, (err, data) => {
+
+  pool.query(`SELECT * FROM transaction ORDER BY merchant ASC`, (err, data) => {
     if (err) {
       next(err);
     } else {
@@ -14,7 +15,7 @@ router.get("/get/transaction", (req, res, next) => {
 
 router.post("/post/transaction", (req, res, next) => {
   const { merchant, payment_type, amount } = req.query;
-
+  console.log(req.query);
   pool.query(
     `INSERT INTO transaction (merchant, payment_type, amount) VALUES ($1, $2, $3)`,
     [merchant, payment_type, amount],
@@ -27,9 +28,26 @@ router.post("/post/transaction", (req, res, next) => {
       Payment: ${req.query.payment_type}
       Amount: ${req.query.amount}`);
         console.log("1 record inserted");
+        
       }
     }
   );
+});
+
+
+router.delete("/delete/transaction:id", (req, res, next) => {
+  console.log("Is the issue here")
+  const id = parseInt(req.params.uid)
+  console.log("Is the issue here")
+  console.log(id);
+  pool.query(
+    `DELETE FROM transaction WHERE uid = $1`, [id],
+     (error, response) =>{
+      if (error) {
+        throw error;
+      }
+      response.status(200).send("Sucesss!");
+    });
 });
 
 router.get("*", (req, res) =>
