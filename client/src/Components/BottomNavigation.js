2080dce0440f666dcb372ec681e19638a50c5fe1
martinezfran63 <martinezfran63@gmail.com>
2020-01-07 16:02:36 -0700

    bottom nav

--- client/src/Components/BottomNavigation.js
+++ client/src/Components/BottomNavigation.js
@@ -2,11 +2,24 @@ import React from "react";
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
+import Divider from '@material-ui/core/Divider';
+
 
 const StyledBottomNavigation = styled(BottomNavigation)`
   && {
     background-color: #00cdbe;
-    height: 70px;
+    height: 200px;
+    color: white;
+  }
+`;
+
+const StyledImage = styled.img`
+  && {
+    display: block;
+    max-width:50px;
+    max-height:50px;
+    width: auto;
+    height: auto;
   }
 `;
 
@@ -15,7 +28,8 @@ export default function BottomNav(){
     return (
       <div>
           <StyledBottomNavigation>
-            Bottom Nav
+                 <StyledImage src='logo_inverse.png' alt="logo" />
+                 <h3> 2019 Eleftheria</h3>
           </StyledBottomNavigation>
       </div>
     );
