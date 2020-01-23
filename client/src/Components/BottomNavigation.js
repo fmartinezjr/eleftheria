ec5201329dbfd236591d95f823a927e15f9dbc36
martinezfran63 <martinezfran63@gmail.com>
2020-01-23 12:59:59 -0700

    footer

--- client/src/Components/BottomNavigation.js
+++ client/src/Components/BottomNavigation.js
@@ -2,35 +2,57 @@ import React from "react";
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
-import Divider from '@material-ui/core/Divider';
 
 
 const StyledBottomNavigation = styled(BottomNavigation)`
   && {
-    background-color: #00cdbe;
-    height: 200px;
-    color: white;
+
   }
 `;
 
-const StyledImage = styled.img`
+const H1 = styled.h1`
   && {
-    display: block;
-    max-width:50px;
-    max-height:50px;
-    width: auto;
-    height: auto;
+    color: white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    font-size: 60px;
+    text-align: center;
+    margin-top: 50px;
   }
 `;
 
+const H2 = styled.h2`
+&& {
+  color: white;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+   font-size: 20px;
+   text-align: center;
+   margin-top: -50px;
+}
+`;
+
+const FooterDiV = styled.div`
+&& {
+  border-width: 15px;
+  background: #00cdbe; 
+  position:relative; 
+  bottom:0; 
+  height:100%;
+  width:100%; 
+  height:180px; 
+}
+`;
 
 export default function BottomNav(){
     return (
-      <div>
-          <StyledBottomNavigation>
-                 <StyledImage src='logo_inverse.png' alt="logo" />
-                 <h3> 2019 Eleftheria</h3>
-          </StyledBottomNavigation>
-      </div>
+      <StyledBottomNavigation>
+        <FooterDiV>
+          <H1 class="text">eleftheria</H1>
+          <H2 class="text2">&copy; 2020</H2>
+        </FooterDiV>
+      </StyledBottomNavigation>
     );
 }
