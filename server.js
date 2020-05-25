8ae368945fac695c41a750cf2896e0f0a54314f6
martinezfran63 <martinezfran63@gmail.com>
2020-05-25 15:37:23 -0600

    remove unnecessary dependencies

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
 var express = require("express");
-var createError = require("http-errors");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 var indexRouter = require("./routes/routes");
 var authRouter = require("./routes/auth");
 var app = express();
 var PORT = process.env.PORT;
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "/build")));
