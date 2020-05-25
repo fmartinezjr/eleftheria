7174332620a23f1d80dd04540ae15c21ef205fcc
martinezfran63 <martinezfran63@gmail.com>
2020-05-25 17:35:31 -0600

    get endpoints working

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,72 @@
-var express = require("express");
-var router = express.Router();
-var pool = require("../db");
+/**
+ * Required External Modules
+ */
 
-router.get('/', function (req, res) {
-    res.send('Hello Auth!');
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const util = require("util");
+const url = require("url");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
+/**
+ * Routes Definitions
+ */
+
+router.get(
+    "/login",
+    passport.authenticate("auth0", {
+      scope: "openid email profile"
+    }),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
+
+router.get("/callback", (req, res, next) => {
+  passport.authenticate("auth0", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      const returnTo = req.session.returnTo;
+      delete req.session.returnTo;
+      res.redirect(returnTo || "/");
+    });
+  })(req, res, next);
+});
+
+router.get("/logout", (req, res) => {
+    req.logOut();
+  
+    let returnTo = req.protocol + "://" + req.hostname;
+    const port = req.connection.localPort;
+  
+    if (port !== undefined && port !== 80 && port !== 443) {
+      returnTo =
+        process.env.NODE_ENV === "production"
+          ? `${returnTo}/`
+          : `${returnTo}:${port}/`;
+    }
+  
+    const logoutURL = new URL(
+      util.format("https://%s/logout", process.env.AUTH0_DOMAIN)
+    );
+    const searchString = querystring.stringify({
+      client_id: process.env.AUTH0_CLIENT_ID,
+      returnTo: returnTo
+    });
+    logoutURL.search = searchString;
+  
+    res.redirect(logoutURL);
   });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,71 @@
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
 require("dotenv").config();
-var express = require("express");
-var path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+/**
+ *  App Configuration
+ */
+
 var indexRouter = require("./routes/routes");
 var authRouter = require("./routes/auth");
-var app = express();
-var PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,8 +73,24 @@ app.use(express.urlencoded({ extended: false }));
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "/build")));
 
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 app.use("/api", indexRouter);
 app.use("/auth", authRouter);
+
+
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
